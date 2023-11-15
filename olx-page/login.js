cc1ce81aa4b82ad199669d3eda9dc4216ad93a7c
Gautam <gautamy499@gmail.com>
2023-11-15 19:10:33 +0530

    add homepage url

--- olx-page/login.js
+++ olx-page/login.js
@@ -2,3 +2,8 @@ const registration = document.getElementById('registration');
 registration.addEventListener('click', () => {
     location.href = './createAcc.html';
 });
+
+const homePage = document.querySelector('.homepage');
+homePage.addEventListener('click', () => {
+    location.href = '../index.html';
+});
